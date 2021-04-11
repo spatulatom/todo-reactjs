e31037ea425b9ae85f6a86923fa6f434b193870d
spatulatom <spatulatom@gmail.com>
2021-04-11 20:09:24 +0100

    Updates

---
+++ precache-manifest.44ed654415d82754e54ef52939ffad0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53dec11caf0f7d5f2076d43d19f6038a",
+    "url": "/Todo-React/index.html"
+  },
+  {
+    "revision": "4feb62d8035c35ef7898",
+    "url": "/Todo-React/static/css/main.1507005f.chunk.css"
+  },
+  {
+    "revision": "262c788b11e23e4bffdf",
+    "url": "/Todo-React/static/js/2.84396668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-React/static/js/2.84396668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4feb62d8035c35ef7898",
+    "url": "/Todo-React/static/js/main.4590d51c.chunk.js"
+  },
+  {
+    "revision": "98284c2f565bfe545027",
+    "url": "/Todo-React/static/js/runtime-main.b014b2e6.js"
+  }
+]);
